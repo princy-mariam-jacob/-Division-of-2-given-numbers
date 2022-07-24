68d91ae6b92ddfd276af74d3ddc0f21cbef4b1be
princy-mariam-jacob <93709324+princy-mariam-jacob@users.noreply.github.com>
2022-07-24 23:14:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ import streamlit as st
 st.title('Divison of 2 numbers')
 a = st.number_input('Enter a number')
 b = st.number_input('Enter another number')
-result = a / b
-st.write(a, ' / ', b , '= ', result)
+if b!=0:
+  result = a / b
+  st.write(a, ' / ', b , '= ', result)
+else:
+  st.write('Enter denominator other than 0')
